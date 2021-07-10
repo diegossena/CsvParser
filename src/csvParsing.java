6019764b38717200aa375d69be2a2f5069458aa2
Diego Sena <46661853+diegossena@users.noreply.github.com>
2021-07-10 10:52:58 -0300

    Update csvParsing.java

--- src/csvParsing.java
+++ src/csvParsing.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 import java.io.IOException;
 
-public class csvParsing {
+public class csvParser {
 
     public static void main(String[] args) throws IOException {
         byte[] lines = Files.readAllBytes(Paths.get("sample.csv"));
