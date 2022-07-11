04c8665c2e0ada60605f0e9ba5b6d3edd5c14fb4
Diego Sena <46661853+diegossena@users.noreply.github.com>
2022-07-11 15:03:42 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 CLS
-"C:\Program Files\Java\jdk-11.0.7\bin\javac" -d classes src\*.java
-"C:\Program Files\Java\jdk-11.0.7\bin\java" -cp classes csvParsing
+"%JAVA_HOME%\bin\javac" -d classes src\*.java
+"%JAVA_HOME%\bin\java" -cp classes csvParsing
